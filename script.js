6b593c5850f2e8718927bc6611dd89e33b771194
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2024-11-14 16:14:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("menu-toggle").onclick = function() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
+};
